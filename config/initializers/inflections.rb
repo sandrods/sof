f9b6ceb16554d22cc2972a4552da33c9a851d76c
Sandro Duarte <sandrods@gmail.com>
2011-06-10 00:22:15 -0300

    inflections mudaram no rails 3.0.7

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -2,9 +2,21 @@
 
 # Add new inflection rules using the following format
 # (all these examples are active by default):
-# ActiveSupport::Inflector.inflections do |inflect|
-#   inflect.plural /^(ox)$/i, '\1en'
-#   inflect.singular /^(ox)en/i, '\1'
-#   inflect.irregular 'person', 'people'
-#   inflect.uncountable %w( fish sheep )
-# end
+ActiveSupport::Inflector.inflections do |inflect|
+
+  inflect.plural /^(.*)ao$/i, '\1oes'
+  inflect.singular /^(.*)oes$/i, '\1ao'
+
+  inflect.plural(/^(.*)m$/i, '\1ns')
+  inflect.singular(/^(.*)ns$/i, '\1m')
+
+  # inflect.plural(/^(.*)r$/i, '\1res')
+  # inflect.singular(/^(.*)res$/i, '\1r')
+
+  inflect.plural(/^(.*)il$/i, '\1is')
+  inflect.singular(/^(.*)is$/i, '\1il')
+
+  inflect.plural(/^(.*)ia$/i, '\1ias')
+  inflect.singular(/^(.*)ias$/i, '\1ia')
+
+end
