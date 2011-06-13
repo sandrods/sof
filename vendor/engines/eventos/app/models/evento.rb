f8575b22a9445fa2b8e2b50dfc97eeb379a82cc4
Sandro Duarte <sandrods@gmail.com>
2011-06-12 23:23:38 -0300

    Agenda de Eventos na Home Page

--- vendor/engines/eventos/app/models/evento.rb
+++ vendor/engines/eventos/app/models/evento.rb
@@ -5,5 +5,10 @@ class Evento < ActiveRecord::Base
   acts_as_taggable_on :tags
   
   validates :titulo, :presence => true, :uniqueness => true
-  
+  validates :inicio, :presence => true
+
+  def self.ultimos
+    Evento.order("inicio desc").limit(3)
+  end
+
 end
