4d6e531afd36c3eb0bd83f3f8b26ce38e2fd037c
Lênio Ferretto <lenio@itbox.com.br>
2011-06-18 15:34:39 -0300

    Nova alteração de sintaxe no sql de tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,10 +1,10 @@
 class Tag < ActsAsTaggableOn::Tag
   
   def self.tags(options = {})
-    query = "select tags.id, name, count(*) as count"
+    query = "select tags.id, tags.name, count(*) as count"
     query << " from taggings, tags"
     query << " where tags.id = tag_id"
-    query << " group by tags.id"
+    query << " group by tags.id,tags.name"
     query << " order by #{options[:order]}" if options[:order] != nil
     query << " limit #{options[:limit]}" if options[:limit] != nil
     tags = Tag.find_by_sql(query)
