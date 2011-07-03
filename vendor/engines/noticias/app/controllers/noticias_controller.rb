5b221a0567e494077d1bead72e6e1044af7d09c2
Sandro Duarte <sandrods@gmail.com>
2011-07-02 23:23:58 -0300

    filtro de notícias

--- vendor/engines/noticias/app/controllers/noticias_controller.rb
+++ vendor/engines/noticias/app/controllers/noticias_controller.rb
@@ -1,15 +1,20 @@
 class NoticiasController < ApplicationController
 
-  before_filter :find_all_noticias
   before_filter :find_page
 
   def index
+    if params[:filtro]
+      @noticias = Noticia.with_query(params[:filtro]).order("data DESC")
+    else
+      find_all_noticias
+    end
     # you can use meta fields from your model instead (e.g. browser_title)
     # by swapping @page for @noticia in the line below:
     present(@page)
   end
 
   def show
+    find_all_noticias
     @noticia = Noticia.find(params[:id])
 
     # you can use meta fields from your model instead (e.g. browser_title)
